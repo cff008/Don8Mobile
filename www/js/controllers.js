da840c9c0136325637b7b7751abc6bc64ffc4df1
cff008 <cfoley3@wisc.edu>
2016-03-15 16:36:21 -0500

    First

---
+++ www/js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('app.controllers', [])
+     
+.controller('myProfileCtrl', function($scope) {
+
+})
+   
+.controller('eventFeedCtrl', function($scope) {
+
+})
+   
+.controller('myEventsCtrl', function($scope) {
+
+})
+   
+.controller('settingsCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('editProfileCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,94 @@
+angular.module('app.routes', ['ionicUIRouter'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('tabsController.myProfile', {
+    url: '/MyProfile',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/myProfile.html',
+        controller: 'myProfileCtrl'
+      }
+    }
+  })
+
+  /* 
+    The IonicUIRouter.js UI-Router Modification is being used for this route.
+    To navigate to this route, do NOT use a URL. Instead use one of the following:
+      1) Using the ui-sref HTML attribute:
+        ui-sref='tabsController.eventFeed'
+      2) Using $state.go programatically:
+        $state.go('tabsController.eventFeed');
+    This allows your app to figure out which Tab to open this page in on the fly.
+    If you're setting a Tabs default page or modifying the .otherwise for your app and
+    must use a URL, use one of the following:
+      /page1/tab2/EventFeed
+      /page1/tab3/EventFeed
+  */
+  .state('tabsController.eventFeed', {
+    url: '/EventFeed',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/eventFeed.html',
+        controller: 'eventFeedCtrl'
+      },
+      'tab3': {
+        templateUrl: 'templates/eventFeed.html',
+        controller: 'eventFeedCtrl'
+      }
+    }
+  })
+
+  .state('myEvents', {
+    url: '/MyEvents',
+    templateUrl: 'templates/myEvents.html',
+    controller: 'myEventsCtrl'
+  })
+
+  .state('tabsController.settings', {
+    url: '/Settings',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/settings.html',
+        controller: 'settingsCtrl'
+      }
+    }
+  })
+
+  .state('login', {
+    url: '/page9',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/SignUp',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('editProfile', {
+    url: '/EditProfile',
+    templateUrl: 'templates/editProfile.html',
+    controller: 'editProfileCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page9')
+
+  
+
+});
