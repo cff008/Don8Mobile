22dfa8dfbf0d9617619f286f4c6793b615c390fa
Josh Graf <josh.graf1@gmail.com>
2016-04-05 17:06:02 -0500

    login testing works

--- karma.conf.js
+++ karma.conf.js
@@ -27,8 +27,8 @@ module.exports = function(config) {
 	   'www/js/directives.js',
 	   'www/index.html',
 	   'www/templates/*.html',
-	   
-	   'www/Test/spec/PlayerSpec.js'
+
+	   'www/Test/spec/*.js'
     ],
 
 
