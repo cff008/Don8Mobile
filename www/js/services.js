3b38dcb23b6486bd3f4a9d868d77d9233ab17707
Josh Graf <josh.graf1@gmail.com>
2016-04-14 02:18:09 -0500

    Fixed Proxy and Updated Logo and Name for Cordova Export

--- www/js/services.js
+++ www/js/services.js
@@ -14,16 +14,9 @@ angular.module('app.services', [])
           var deferred = $q.defer();
           var promise = deferred.promise;
 
-          //TODO: replace these checks with calls to the server
-          /*if (email == 'user' && pw == 'secret') {
-              deferred.resolve('Welcome ' + email + '!');
-          } else {
-              deferred.reject('Wrong credentials.');
-          }*/
-
           $http({
             method: 'GET',
-            url: '/login',
+            url: 'http://www.don8don8.site/data/login.php',
             params: {email: email, password: pw}
           }).then(function successCallback(response) {
             //this callback will be called asynchronously when the response is available
@@ -59,7 +52,7 @@ angular.module('app.services', [])
     var deferred = $q.defer();
     var promise = $http({
       method: 'GET',
-      url: '/profile',
+      url: 'http://don8don8.site/data/displayprofile.php',
       params: {id: $rootScope.userid}
     }).then(function successCallback(response) {
       if(response.data.status == 'OK'){
@@ -87,7 +80,7 @@ angular.module('app.services', [])
 
       $http({
         method: 'GET',
-        url: '/update_profile',
+        url: 'http://don8don8.site/data/update_profile.php',
         params: {firstname: firstname, lastname: lastname, email: email, interests: interests}
       }).then(function successCallback(response) {
         //this callback will be called asynchronously when the response is available
@@ -155,7 +148,7 @@ angular.module('app.services', [])
 
       $http({
         method: 'GET',
-        url: '/create_profile',
+        url: 'http://www.don8don8.site/data/create_new_profile.php',
         params: {firstname: firstname, lastname: lastname, email: email, password: password}
       }).then(function successCallback(response) {
         //this callback will be called asynchronously when the response is available
@@ -196,7 +189,7 @@ angular.module('app.services', [])
 
     $http({
       method: 'GET',
-      url: '/get_settings',
+      url: 'http://www.don8don8.site/data/settings.php',
       params: {id: $rootScope.userid}
     }).then(function successCallback(response) {
       if(response.data.status == 'OK'){
@@ -260,98 +253,10 @@ angular.module('app.services', [])
   }
 })
 
-.service('LoginService', function($q, $http, $rootScope) {
-  return {
-      loginUser: function(email, pw) {
-          var deferred = $q.defer();
-          var promise = deferred.promise;
-
-          //TODO: replace these checks with calls to the server
-          /*if (email == 'user' && pw == 'secret') {
-              deferred.resolve('Welcome ' + email + '!');
-          } else {
-              deferred.reject('Wrong credentials.');
-          }*/
-
-          $http({
-            method: 'GET',
-            url: '/login',
-            params: {email: email, password: pw}
-          }).then(function successCallback(response) {
-            //this callback will be called asynchronously when the response is available
-            if(response.data.status == 'OK'){
-              deferred.resolve('Welcome ' + email + '!');
-              $rootScope.userid = response.data.userid;
-            } else if(response.data.status == 'INVALID_REQUEST') {
-              deferred.reject('Username or password is incorrect');
-            } else if(response.data.status == 'UNKNOWN_ERROR') {
-              deferred.reject('Something went wrong. Please try again.');
-            } else {
-              deferred.reject('This shouldn\'t happen');
-            }
-          }, function errorCallback(response) {
-            //called asynchronously if an error occurs or server returns response with an error status
-            deferred.reject('Server communication error');
-          });
-          promise.success = function(fn) {
-              promise.then(fn);
-              return promise;
-          }
-          promise.error = function(fn) {
-              promise.then(null, fn);
-              return promise;
-          }
-          return promise;
-      }
-  }
-})
-
-.service('SignupService', function($q, $http, $rootScope) {
-  return{
-    signupUser: function(firstname, lastname, email, password) {
-      var deferred = $q.defer();
-      var promise = deferred.promise;
-
-      $http({
-        method: 'GET',
-        url: '/create_profile',
-        params: {firstname: firstname, lastname: lastname, email: email, password: password}
-      }).then(function successCallback(response) {
-        //this callback will be called asynchronously when the response is available
-        if(response.data.status == 'OK'){
-          deferred.resolve('Welcome ' + firstname + ' ' + lastname + '!');
-          $rootScope.userid = response.data.userid;
-        } else if(response.data.status == 'UNKNOWN_ERROR') {
-          deferred.reject('Something went wrong. Please try again.');
-        } else if(response.data.status == 'INVALID_REQUEST'){
-          deferred.reject('That email address is already taken');
-        } else {
-          deferred.reject('This shouldn\'t happen');
-        }
-
-        //TODO: Add cases for: username/email already exists, etc.
-      }, function errorCallback(response) {
-        //called asynchronously if an error occurs or the server returns response with an error status
-        deferred.reject('Server communication error');
-      });
-
-      promise.success = function(fn) {
-        promise.then(fn);
-        return promise;
-      }
-      promise.error = function(fn) {
-        promise.then(null, fn);
-        return promise;
-      }
-      return promise;
-    }
-  }
-})
-
 .service('dataService', [ '$http', '$q',
     function ($http, $q) { return {
       getEvents: function(){
-		  return $http.get('/events/all').then(function(resp){
+		  return $http.get('http://don8don8.site/data/events.php').then(function(resp){
 			if(typeof resp.data === 'object'){
 			  this.events = resp.data.events;
 				console.log(this.events);
@@ -367,7 +272,7 @@ angular.module('app.services', [])
 	getEvent: function(id){
 		return $http({
       method: 'GET',
-      url: '/events/all',
+      url: 'http://don8don8.site/data/events.php',
       params: {id: id}
     }).then(function successCallback(response) {
 		if(response.data.status == 'OK'){
@@ -386,7 +291,7 @@ angular.module('app.services', [])
 	getMyEvents: function(id){
 		return $http({
       method: 'GET',
-      url: '/event',
+      url: 'http://don8don8.site/data/events.php',
       params: {userid: id}
     }).then(function successCallback(response) {
 		if(response.data.status == 'OK'){
